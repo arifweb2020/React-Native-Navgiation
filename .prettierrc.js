17251f596fc9b5011c05adc3168f08ac241acc22
arifweb2020 <arifhussainb2019@gmail.com>
2022-08-14 20:30:15 +0530

    backend api

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,11 @@
 module.exports = {
-  bracketSpacing: false,
-  jsxBracketSameLine: true,
-  singleQuote: true,
-  trailingComma: 'all',
-  arrowParens: 'avoid',
+  // arrowParens: 'avoid',
+  // bracketSameLine: true,
+  // bracketSpacing: false,
+  // singleQuote: true,
+  // trailingComma: 'all',
+  
+  trailingComma: "all",
+  tabWidth: 4,
+  singleQuote: "avoid",
 };
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,13 +1,27 @@
-import React from 'react';
-import {View, Text, TouchableOpacity} from 'react-native';
-import {useNavigation} from '@react-navigation/native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 export default function Home() {
   const navigation = useNavigation();
+  // const [data, setData] = useState([])
+
+  // useEffect(() => {
+  //   const getData = async () => {
+  //     const res = await fetch('http://10.0.2.2:5000/articles');
+  //     const res1 = await res.json();
+  //     console.log('res1 ' + res1);
+  //     setData(res1)
+  //   };
+  //   getData();
+  // }, []);
 
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <Text>Home</Text>
+      {/* {data.map(ele => {
+        return <Text>{ele.authorname}</Text>
+      })} */}
 
       <TouchableOpacity
         style={{marginTop: 20}}
